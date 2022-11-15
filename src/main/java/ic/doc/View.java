6bff5fed49c948a735265724f655fc0cc4dd3080
rob <rgw20@ic.ac.uk>
2022-11-15 16:39:32 +0000

    Apply observer pattern. Start to implement MVC

--- src/main/java/ic/doc/View.java
+++ src/main/java/ic/doc/View.java
@@ -1,41 +1,38 @@
 package ic.doc;
 
 import javax.swing.*;
+import java.awt.event.ActionListener;
 
-public class RPCgui {
-  private void display() {
+public class View {
+
+  private final JTextField txtField = new JTextField(10);
+
+  public View (ActionListener controller) {
     JFrame frame = new JFrame("Reverse Polish Calculator");
     frame.setSize(350, 200);
 
     JPanel panel = new JPanel();
 
-    final JTextField txtField = new JTextField(10);
-
     for (int i = 1; i < 5; i++) {
       JButton button = new JButton(String.valueOf(i));
-      int finalI = i;
-      button.addActionListener(e -> txtField.setText(String.valueOf(finalI)));
+      button.addActionListener(controller);
       panel.add(button);
     }
     JButton addButton = new JButton("+");
-    addButton.addActionListener(e -> txtField.setText("+"));
+    addButton.addActionListener(controller);
     panel.add(addButton);
     JButton subButton = new JButton("-");
-    subButton.addActionListener(e -> txtField.setText("-"));
+    subButton.addActionListener(controller);
     panel.add(subButton);
 
     panel.add(txtField);
 
     JButton clearButton = new JButton("Clear");
-    clearButton.addActionListener(e -> txtField.setText(""));
+    clearButton.addActionListener(controller);
     panel.add(clearButton);
 
     frame.getContentPane().add(panel);
     frame.setVisible(true);
-  }
-
-  public static void main(String[] args) {
-    new RPCgui().display();
   }
 
 }
